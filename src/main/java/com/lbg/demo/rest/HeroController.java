fdf61a628b155fa64e0ade8da68edb92d471217b
liammcnabola <liam.mcnabola@protonmail.com>
2024-01-24 10:28:23 +0000

    following on with hero before ex.

---
+++ src/main/java/com/lbg/demo/rest/HeroController.java
@@ -0,0 +1,22 @@
+package com.lbg.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lbg.demo.domain.Hero;
+
+@RestController
+public class HeroController {
+
+	@GetMapping("/hero")
+	public String greeting() {
+		return "Superhero!";
+	}
+
+	@PostMapping("/create")
+	public String createHero(@RequestBody Hero newHero) {
+		return newHero.toString();
+	}
+}
